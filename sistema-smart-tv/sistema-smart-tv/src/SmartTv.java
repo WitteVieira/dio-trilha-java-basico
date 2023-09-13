5c937d4295fdc5d6c77b674bbd6d7a973d536931
Witte Vieira de Carvalho <witte.vieira.carvalho@gmail.com>
2023-09-13 16:44:04 -0300

    TV smart volume

--- sistema-smart-tv/sistema-smart-tv/src/SmartTv.java
+++ sistema-smart-tv/sistema-smart-tv/src/SmartTv.java
@@ -1,6 +1,18 @@
 public class SmartTv {
-    boolean ligada=false;
-    int canal=1;
+    boolean ligada = false;
+    int canal = 1;
     int volume = 25;
+
+    public void aumentarVolume() {
+        volume++;
     }
+
+    public void ligar() {
+        ligada = true;
+    }
+    public void desligar() {
+        ligada = false;
+    }
+}
+
 
